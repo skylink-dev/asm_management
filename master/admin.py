2bd0dbed1d4df2a95d7e6511c5890950ad568122
Arulraj <jarulraj2@gmail.com>
2025-09-09 12:38:22 +0530

    zone added

--- master/admin.py
+++ master/admin.py
@@ -1,7 +1,32 @@
 from django.contrib import admin
-from .models import Zone
+from .models import Zone, State, District, Taluk
 
 @admin.register(Zone)
 class ZoneAdmin(admin.ModelAdmin):
-    list_display = ("id", "name", "code", "description")
-    search_fields = ("name", "code")
+    list_display = ('id', 'name', 'code', 'description')
+    search_fields = ('name', 'code')
+    list_per_page = 20
+
+
+@admin.register(State)
+class StateAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'code', 'zone', 'description')
+    search_fields = ('name', 'code', 'zone__name')
+    list_filter = ('zone',)
+    list_per_page = 20
+
+
+@admin.register(District)
+class DistrictAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'code', 'state', 'description')
+    search_fields = ('name', 'code', 'state__name')
+    list_filter = ('state',)
+    list_per_page = 20
+
+
+@admin.register(Taluk)
+class TalukAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'code', 'district', 'description')
+    search_fields = ('name', 'code', 'district__name')
+    list_filter = ('district',)
+    list_per_page = 20
--- master/models.py
+++ master/models.py
@@ -1,13 +1,57 @@
 from django.db import models
 
 class Zone(models.Model):
-    name = models.CharField(max_length=100, unique=True, default="Default Zone")
-    code = models.CharField(max_length=20, unique=True, default="ZONE001")
-    description = models.TextField(blank=True, null=True, default="No description available")
+    name = models.CharField(max_length=100, unique=True, default="Unknown Zone")
+    code = models.CharField(max_length=20, unique=True, default="Z001")
+    description = models.TextField(blank=True, null=True, default="No description")
 
     class Meta:
         verbose_name = "Zone"
         verbose_name_plural = "Zones"
+
+    def __str__(self):
+        return self.name
+
+
+class State(models.Model):
+    name = models.CharField(max_length=100, unique=True, default="Unknown State")
+    code = models.CharField(max_length=20, unique=True, default="S001")
+    zone = models.ForeignKey(Zone, on_delete=models.CASCADE, related_name="states", default=1)
+    description = models.TextField(blank=True, null=True, default="No description")
+
+    class Meta:
+        verbose_name = "State"
+        verbose_name_plural = "States"
+
+    def __str__(self):
+        return self.name
+
+
+class District(models.Model):
+    name = models.CharField(max_length=100, default="Unknown District")
+    code = models.CharField(max_length=20, default="D001")
+    state = models.ForeignKey(State, on_delete=models.CASCADE, related_name="districts", default=1)
+    description = models.TextField(blank=True, null=True, default="No description")
+
+    class Meta:
+        unique_together = ('name', 'state')
+        verbose_name = "District"
+        verbose_name_plural = "Districts"
+
+    def __str__(self):
+        return self.name
+
+
+class Taluk(models.Model):
+    name = models.CharField(max_length=100, default="Unknown Taluk")
+    code = models.CharField(max_length=20, default="T001")
+    district = models.ForeignKey(District, on_delete=models.CASCADE, related_name="taluks", default=1)
+    description = models.TextField(blank=True, null=True, default="No description")
+
+    class Meta:
+        unique_together = ('name', 'district')
+        verbose_name = "Taluk"
+        verbose_name_plural = "Taluks"
 
     def __str__(self):
         return self.name
